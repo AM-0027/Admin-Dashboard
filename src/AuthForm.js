a077eb1644f6a73471bb40a1bf35f12567f99033
Your Name <your-email@example.com>
2025-05-12 22:30:03 +0530

    add tostify

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { Bounce } from "react-toastify/unstyled";
+
 
 export default function AuthForm() {
   const [isLogin, setIsLogin] = React.useState(true);
+  const [email, setemail] = React.useState("");
+  const [password, setpassword] = React.useState("");
+  const [confirmedpassword, setconfirmedpassword] = React.useState("");
+
+  const validateEmail = (email) => {
+    const pattren = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattren.test(email);
+  };
+
+  const handlelogin = () => {
+    if (!validateEmail(email)) {
+      toast.error("INVALID EMAIL");
+    } else if (password.length < 6) {
+      toast.error("PASSWORD LENGTH MUST ME 6 LETTERS");
+    } else {
+      toast.success("LOGIN SUCCESSFULLY:>");
+    }
+  };
+
+  const handleSignUp = () => {
+    if (!validateEmail(email)) {
+      toast.error("INVALID EMAIL");
+    } else if (password.length < 6) {
+      toast.error("PASSWORD MUST BE 6 LETTERS");
+    } else if (password !== confirmedpassword) {
+      toast.error("PASSWORD NOT MATCH");
+    } else {
+      toast.success("SIGNUP SUCCESSFULLY:>");
+    }
+  };
+
   return (
     <div className="container">
       <div className="form-container">
@@ -23,10 +58,22 @@ export default function AuthForm() {
           <>
             <div className="form">
               <h2>Login Form</h2>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
+              />
               <a href="#">Forget Password?</a>
-              <button>Login</button>
+              <button className="toast" onClick={handlelogin}>
+                Login
+              </button>
               <p>
                 Not a Member?{" "}
                 <a href="#" onClick={() => setIsLogin(false)}>
@@ -38,15 +85,43 @@ export default function AuthForm() {
         ) : (
           <>
             <div className="form">
-              <h2>SignUp Form</h2>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Confirm Password" />
-              <button>SignUp</button>
-            </div>
+            <h2>SignUp Form</h2>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmedpassword}
+              onChange={(e) => setconfirmedpassword(e.target.value)}
+            />
+            <button onClick={handleSignUp}>SignUp</button>
+          </div>
           </>
         )}
       </div>
+      <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition={Bounce}
+              />
     </div>
   );
 }
